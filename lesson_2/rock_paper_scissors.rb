77f6506311e932a61e0f24fbbbe7b2fe286996ec
21sapphire <21sapphiremusic@gmail.com>
2021-10-24 08:22:20 -0500

    commit

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -4,17 +4,19 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def display_results(player,computer)
-  if (player == 'rock' && computer == 'scissors') ||
-    (player == 'paper' && computer == 'rock') ||
-    (player == 'scissors' && computer == 'paper')
-  prompt("You won!")
-  elsif (player == 'rock' && computer == 'paper') ||
-      (player == 'paper' && computer == 'scissors') ||
-      (player == 'scissors' && computer == 'rock')
-  prompt('Computer won!')
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt('Computer won!')
   else
-  prompt("It's a tie!")
+    prompt("It's a tie!")
   end
 end
 
@@ -33,9 +35,9 @@ loop do
 
   computer_choices = VALID_CHOICES.sample
 
-  prompt("You chose: #{choice}; Computer chose: #{computer}")
+  prompt("You chose: #{choice}; computer chose: #{computer_choices}")
 
-  display_results(choice, computer)
+  display_results(choice, computer_choices)
 
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
